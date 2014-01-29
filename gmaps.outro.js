8565fd05abbf810f2c24749b0ed877cfe64c6ecc
Jensen Tonne <jstonne@stackideas.com>
2014-01-29 16:50:22 +0800

    Get google maps language code from meta tag.

--- gmaps.outro.js
+++ gmaps.outro.js
@@ -3,9 +3,11 @@
 
 });
 
+var language = $('meta[property="foundry:location:language"]').attr("content") || 'en';
+
 // Load google maps api
 $.require()
-  .script($.uri(window.location).protocol() + "://maps.google.com/maps/api/js?sensor=true&callback=" + callbackId)
+  .script($.uri(window.location).protocol() + "://maps.google.com/maps/api/js?sensor=true&callback=" + callbackId + "&language=" + language)
   .done();
 
 };
