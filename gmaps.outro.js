0c85770f0ecec2fe7e2b4e089afe3b391812d128
Jensen Tonne <jstonne@stackideas.com>
2014-03-20 17:26:41 +0800

    Don't load Google Maps if it has been loaded by others.

--- gmaps.outro.js
+++ gmaps.outro.js
@@ -5,10 +5,15 @@
 
 var language = $('meta[name="foundry:location:language"]').attr("content") || 'en';
 
-// Load google maps api
-$.require()
-  .script($.uri(window.location).protocol() + "://maps.google.com/maps/api/js?sensor=true&callback=" + callbackId + "&language=" + language)
-  .done();
+if (window.google && window.google.maps){
+    window[callbackId]();
+} else {
+    // Load google maps api
+    $.require()
+      .script($.uri(window.location).protocol() + "://maps.google.com/maps/api/js?sensor=true&callback=" + callbackId + "&language=" + language)
+      .done();
+}
+
 
 };
 // module factory: end
