daac2d2775318a117771000bcc4d7613565be1fc
Mark Lee <mark@stackideas.com>
2015-11-27 16:59:45 +0800

    Use https by default for static maps

--- gmaps.js
+++ gmaps.js
@@ -1540,7 +1540,7 @@
       var parameters = [];
       var data;
 
-      var static_root = 'http://maps.googleapis.com/maps/api/staticmap';
+      var static_root = 'https://maps.googleapis.com/maps/api/staticmap';
       if (options.url){
         static_root = options.url;
         delete options.url;
